d03e8180de2aca6b000419564a79c1ee00953244
Konrad Koschel <konrad.koschel@hotmail.de>
2022-05-22 17:09:40 +0200

    Added Public API for timestamp-based measurement retrieval

---
+++ backend/routes/public/measurement.js
@@ -0,0 +1,51 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/temperature", async (req, res) => {
+    const {deviceID, timestamp} = req.body;
+
+    if (!deviceID || !timestamp) {
+        res.status(400).send("Device ID and Timestamp are mandatory");
+    }
+
+    // TODO
+
+    res.json({});
+});
+
+router.get("/humidity", async (req, res) => {
+    const {deviceID, timestamp} = req.body;
+
+    if (!deviceID || !timestamp) {
+        res.status(400).send("Device ID and Timestamp are mandatory");
+    }
+
+    // TODO
+
+    res.json({});
+});
+
+router.get("/tilt", async (req, res) => {
+    const {deviceID, timestamp} = req.body;
+
+    if (!deviceID || !timestamp) {
+        res.status(400).send("Device ID and Timestamp are mandatory");
+    }
+
+    // TODO
+
+    res.json({});
+});
+router.get("/vibration", async (req, res) => {
+    const {deviceID, timestamp} = req.body;
+
+    if (!deviceID || !timestamp) {
+        res.status(400).send("Device ID and Timestamp are mandatory");
+    }
+
+    // TODO
+
+    res.json({});
+});
+
+export default router;
