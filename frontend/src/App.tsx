15e0e45b003a6e05a2aa6f19176dda1ac2522f19
VanDerBrandung <luisschmitt01@gmail.com>
2022-05-22 18:19:20 +0200

    initial React setup
    - setup project with create-react-app
    - add typescript
    - add tailwind
    - initial folder structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='bg-red-500'>initial setup</h1>
+    </div>
+  );
+}
+
+export default App;
