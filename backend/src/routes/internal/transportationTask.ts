88f92c31eda9feeeeb3a9c680600a7407831f8a5
Konrad Koschel <konrad.koschel@hotmail.de>
2022-05-24 09:37:21 +0200

    Added ESLint for Code Quality (Forget to save some file :D )

--- backend/src/routes/internal/transportationTask.ts
+++ backend/src/routes/internal/transportationTask.ts
@@ -4,65 +4,65 @@ const router = express.Router();
 import model from "../../db/model/transportationTask.js";
 
 router.get("/", async (_, res) => {
-    res.json(await model.find());
+	res.json(await model.find());
 });
 
 router.get("/:id", async (req, res) => {
-    const {id} = req.params;
-    try {
-        const element = await model.findById(id);
-        res.json(element);
-    }
-    catch (error) {
-        res.status(400).json(error);
-    }
+	const {id} = req.params;
+	try {
+		const element = await model.findById(id);
+		res.json(element);
+	}
+	catch (error) {
+		res.status(400).json(error);
+	}
 });
 
 router.post("/", async (req, res) => {
-    const {
-        deviceID, 
-        description, 
-        owner, 
-    } = req.body;
-    const element = await model.create({
-        deviceID, 
-        description, 
-        owner, 
-        measurements: []
-    });
-    res.status(201).json(element);
+	const {
+		deviceID, 
+		description, 
+		owner, 
+	} = req.body;
+	const element = await model.create({
+		deviceID, 
+		description, 
+		owner, 
+		measurements: []
+	});
+	res.status(201).json(element);
 });
 
 router.put("/:id", async (req, res) => {
-    const {id} = req.params;
-    await model.findById(id).catch(res.status(400).json);
+	const {id} = req.params;
+	await model.findById(id).catch(res.status(400).json);
 
-    const {
-        deviceID, 
-        description, 
-        owner
-    } = req.body;
-    const updated = await model.findByIdAndUpdate(
-        id,
-        {
-            deviceID, 
-            description, 
-            owner
-        },
-        {
-            new: true
-        }
-    );
+	const {
+		deviceID, 
+		description, 
+		owner
+	} = req.body;
+	const updated = await model.findByIdAndUpdate(
+		id,
+		{
+			deviceID, 
+			description, 
+			owner
+		},
+		{
+			new: true
+		}
+	);
 
-    res.json(updated);
+	res.json(updated);
 });
 
 router.delete("/:id", async (req, res) => {
-    const {id} = req.params; 
-    await model.findById(id).catch(res.status(400).json);
+	const {id} = req.params; 
+	await model.findById(id).catch(res.status(400).json);
 
-    const deleted = await model.findByIdAndDelete(id);
-    res.status(200).json(deleted);
+	const deleted = await model.findByIdAndDelete(id);
+	res.status(200).json(deleted);
 });
 
 export default router;
--- backend/src/routes/public/measurement.ts
+++ backend/src/routes/public/measurement.ts
@@ -2,54 +2,54 @@ import express from "express";
 const router = express.Router();
 
 router.get("/temperature", async (req, res) => {
-    const {deviceID, timestamp} = req.body;
+	const {deviceID, timestamp} = req.body;
 
-    if (!deviceID || !timestamp) {
-        res.status(400).send("Device ID and Timestamp are mandatory");
-        return;
-    }
+	if (!deviceID || !timestamp) {
+		res.status(400).send("Device ID and Timestamp are mandatory");
+		return;
+	}
 
-    // TODO
+	// TODO
 
-    res.json({});
+	res.json({});
 });
 
 router.get("/humidity", async (req, res) => {
-    const {deviceID, timestamp} = req.body;
+	const {deviceID, timestamp} = req.body;
 
-    if (!deviceID || !timestamp) {
-        res.status(400).send("Device ID and Timestamp are mandatory");
-        return;
-    }
+	if (!deviceID || !timestamp) {
+		res.status(400).send("Device ID and Timestamp are mandatory");
+		return;
+	}
 
-    // TODO
+	// TODO
 
-    res.json({});
+	res.json({});
 });
 
 router.get("/tilt", async (req, res) => {
-    const {deviceID, timestamp} = req.body;
+	const {deviceID, timestamp} = req.body;
 
-    if (!deviceID || !timestamp) {
-        res.status(400).send("Device ID and Timestamp are mandatory");
-        return;
-    }
+	if (!deviceID || !timestamp) {
+		res.status(400).send("Device ID and Timestamp are mandatory");
+		return;
+	}
 
-    // TODO
+	// TODO
 
-    res.json({});
+	res.json({});
 });
 router.get("/vibration", async (req, res) => {
-    const {deviceID, timestamp} = req.body;
+	const {deviceID, timestamp} = req.body;
 
-    if (!deviceID || !timestamp) {
-        res.status(400).send("Device ID and Timestamp are mandatory");
-        return;
-    }
+	if (!deviceID || !timestamp) {
+		res.status(400).send("Device ID and Timestamp are mandatory");
+		return;
+	}
 
-    // TODO
+	// TODO
 
-    res.json({});
+	res.json({});
 });
 
 export default router;
