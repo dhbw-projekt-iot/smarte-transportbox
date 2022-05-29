fb9d87e1429d919d2f682511582955ae7f507bcd
VanDerBrandung <luisschmitt01@gmail.com>
2022-05-29 23:18:06 +0200

    added delivery table

---
+++ frontend/src/components/app/Deliveries/testData.ts
@@ -0,0 +1,74 @@
+export const testData: any = [
+  {
+    id: '1',
+    type: 'Kühlfracht',
+    creationDate: '23.03.2022',
+    status: 'zugestellt',
+  },
+  {
+    id: '2',
+    type: 'Kühlfracht',
+    creationDate: '23.03.2022',
+    status: 'versendet',
+  },
+  {
+    id: '3',
+    type: 'Kühlfracht',
+    creationDate: '23.03.2022',
+    status: 'ausstehend',
+  },
+  {
+    id: '4',
+    type: 'Kühlfracht',
+    creationDate: '23.03.2022',
+    status: 'zugestellt',
+  },
+  {
+    id: '5',
+    type: 'Kühlfracht',
+    creationDate: '23.03.2022',
+    status: 'zugestellt',
+  },
+  {
+    id: '6',
+    type: 'Kühlfracht',
+    creationDate: '23.03.2022',
+    status: 'verloren',
+  },
+  {
+    id: '7',
+    type: 'Fragile Fracht',
+    creationDate: '23.03.2022',
+    status: 'verloren',
+  },
+  {
+    id: '8',
+    type: 'Fragile Fracht',
+    creationDate: '23.03.2022',
+    status: 'zugestellt',
+  },
+  {
+    id: '9',
+    type: 'Kühlfracht',
+    creationDate: '23.03.2022',
+    status: 'ungewiss',
+  },
+  {
+    id: '10',
+    type: 'Verderblich',
+    creationDate: '23.03.2022',
+    status: 'zugestellt',
+  },
+  {
+    id: '12',
+    type: 'Verderblich',
+    creationDate: '23.03.2022',
+    status: 'zugestellt',
+  },
+  {
+    id: '13',
+    type: 'Kühlfracht',
+    creationDate: '23.03.2022',
+    status: 'zugestellt',
+  },
+];
