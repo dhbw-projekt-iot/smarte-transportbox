e7ede3926f70729e7ec6a29533b79ca5d947953b
VanDerBrandung <luisschmitt01@gmail.com>
2022-06-26 18:44:44 +0200

    fix volumes in docker-compose

--- backend/src/db/model/transportationTask.ts
+++ backend/src/db/model/transportationTask.ts
@@ -1,39 +1,39 @@
-import mongoose from "mongoose";
-import constraintsSchema from "./constraints.js";
+import mongoose from 'mongoose';
+import constraintsSchema from './constraints.js';
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const schema = new mongoose.Schema({
-	deviceID: {
-		type: Schema.Types.ObjectId,
-		ref: "Device",
-		required: true,
-	},
-	measurements: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "Measurement"
-		}
-	],
-	incidents: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "Incident"
-		}
-	],
-	constraints: {
-		type: constraintsSchema.schema,
-		required: true
-	},
-	// Metadata
-	productDescription: String,
-	productType: String,
-	shippingID: String,
-	fromLocation: String,
-	toLocation: String,
-	ownerMail: String,
-	createdAt: Date,
-	status: String
+  deviceID: {
+    type: Schema.Types.ObjectId,
+    ref: 'Device',
+    required: false,
+  },
+  measurements: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Measurement',
+    },
+  ],
+  incidents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Incident',
+    },
+  ],
+  constraints: {
+    type: constraintsSchema.schema,
+    required: true,
+  },
+  // Metadata
+  productDescription: String,
+  productType: String,
+  shippingID: String,
+  fromLocation: String,
+  toLocation: String,
+  ownerMail: String,
+  createdAt: Date,
+  status: String,
 });
 
-export default mongoose.model("TransportationTask", schema);
+export default mongoose.model('TransportationTask', schema);
