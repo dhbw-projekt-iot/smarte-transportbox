d737143dded69739353bdf0543f7a9ce53b58824
Konrad Koschel <konrad.koschel@hotmail.de>
2022-07-03 13:39:14 +0200

    Fixed Mail Service

--- backend/src/logic/handleIncident.ts
+++ backend/src/logic/handleIncident.ts
@@ -1,34 +1,38 @@
 import nodemailer from "nodemailer";
 
 export async function handleIncident(ownerMail: string, transportationTask: any, incident: any) {
-	const mailAccount = await nodemailer.createTestAccount();
-	const env = process.env as any;
-
-	const transporter = nodemailer.createTransport({
-		host: env.INCIDENT_MAIL_HOST,
-		port: env.INCIDENT_MAIL_PORT,
-		secure: false,
-		auth: {
-			user: env.INCIDENT_MAIL_USERNAME || mailAccount.user,
-			pass: env.INCIDENT_MAIL_PASSWORD || mailAccount.pass
-		}
-	});
-
-	await transporter.sendMail({
-		from: "noreply@smartetransportbox.com",
-		to: ownerMail,
-		subject: "New Incident on your delivery",
-		text: `
-		Hello,
-
-		An incident occured for your delivery from ${transportationTask.fromLocation} to ${transportationTask.toLocation}!
-
-		It was noticed by your monitoring module with the ID ${transportationTask.deviceID}.
-
-		The ${incident.sensor} sensor reached a value of ${incident.value}.
-
-		Regards,
-		Your SmarteTransportBox Team
-		`
-	});
+	try {
+		const mailAccount = await nodemailer.createTestAccount();
+		const env = process.env as any;
+	
+		const transporter = nodemailer.createTransport({
+			host: env.INCIDENT_MAIL_HOST,
+			port: env.INCIDENT_MAIL_PORT,
+			secure: false,
+			auth: {
+				user: env.INCIDENT_MAIL_USERNAME || mailAccount.user,
+				pass: env.INCIDENT_MAIL_PASSWORD || mailAccount.pass
+			}
+		});
+	
+		await transporter.sendMail({
+			from: "noreply@smartetransportbox.com",
+			to: ownerMail,
+			subject: "New Incident on your delivery",
+			text: `
+			Hello,
+	
+			An incident occured for your delivery from ${transportationTask.fromLocation} to ${transportationTask.toLocation}!
+	
+			It was noticed by your monitoring module with the ID ${transportationTask.deviceID}.
+	
+			The ${incident.sensor} sensor reached a value of ${incident.value}.
+	
+			Regards,
+			Your SmarteTransportBox Team
+			`
+		});
+	} catch (e) {
+		console.error("Sending alert mail failed: ", e);
+	}
 }
